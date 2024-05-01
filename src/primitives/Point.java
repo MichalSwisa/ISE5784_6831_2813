e11312dea1508477f879c7d4aee1d6efed272772
Michal Swisa <miswisa@acad.jct.ac.il>
2024-05-02 00:54:26 +0300

    Add and implement class Point.

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -23,7 +23,7 @@ public class Point {
             throw new IllegalArgumentException("ZERO vector not allowed");
         }
 
-        return new Vector(result);
+        return new Vector(result);//
     }
 
 
