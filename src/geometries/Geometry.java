dbfd476ac50eae7d910a818e15d6c6277a9973d3
Michal Swisa <miswisa@acad.jct.ac.il>
2024-05-08 14:18:26 +0300

    Add description to package geometries.

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -3,7 +3,17 @@ package geometries;
 import primitives.Point;
 import primitives.Vector;
 
+/**
+ * Represents a geometric shape in three-dimensional space.
+ * Defines a method for obtaining the normal vector to the surface at a given point.
+ */
 public interface Geometry {
+    /**
+     * Returns the normal vector to the surface of the geometry at the given point.
+     *
+     * @param point The point on the surface of the geometry.
+     * @return The normal vector to the surface at the given point.
+     */
     Vector getNormal(Point point);
 
 }
