69e0f974dbc2163af7df2ef3159ca88e4fe41b5e
Michal Swisa <miswisa@acad.jct.ac.il>
2024-06-05 11:23:03 +0300

    Add and implement Scene class.

---
+++ src/main/java/org/example/scene/Scene.java
@@ -0,0 +1,75 @@
+package org.example.scene;
+
+import org.example.geometries.Geometries;
+import org.example.lighting.AmbientLight;
+import org.example.primitives.Color;
+
+/**
+ * The Scene class represents a 3D scene containing geometries, background color,
+ * ambient light, and the name of the scene.
+ */
+public class Scene {
+    /**
+     * The name of the scene.
+     */
+    public String name;
+    /**
+     * The background color of the scene. Defaults to black.
+     */
+    public Color background;
+    /**
+     * The ambient light of the scene. Defaults to no ambient light.
+     */
+    public AmbientLight ambientLight;
+    /**
+     * The geometries in the scene. Defaults to an empty set of geometries.
+     */
+    public Geometries geometries;
+
+
+    /**
+     * Constructs a new Scene with the specified name.
+     * Initializes the background to black, ambient light to none, and geometries to an empty set.
+     *
+     * @param name the name of the scene
+     */
+    public Scene(String name) {
+        this.name = name;
+        this.background = Color.BLACK;// Default background color is black
+        this.ambientLight = AmbientLight.NONE;// Default ambient light is NONE
+        this.geometries = new Geometries();// Default is an empty set of geometries
+    }
+
+    /**
+     * Sets the background color of the scene.
+     *
+     * @param background the new background color
+     * @return the current Scene object (this)
+     */
+    public Scene setBackground(Color background) {
+        this.background = background;
+        return this;
+    }
+
+    /**
+     * Sets the ambient light of the scene.
+     *
+     * @param ambientLight the new ambient light
+     * @return the current Scene object (this)
+     */
+    public Scene setAmbientLight(AmbientLight ambientLight) {
+        this.ambientLight = ambientLight;
+        return this;
+    }
+
+    /**
+     * Sets the geometries of the scene.
+     *
+     * @param geometries the new set of geometries
+     * @return the current Scene object (this)
+     */
+    public Scene setGeometries(Geometries geometries) {
+        this.geometries = geometries;
+        return this;
+    }
+}
